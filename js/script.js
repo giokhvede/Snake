06a545ef84a6ef10bd9d9176b812f1a8d5b05b14
giokhvede <148794939+giokhvede@users.noreply.github.com>
2024-02-12 19:53:11 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,231 @@
+const board = document.getElementById("game-board");
+const instructionText = document.getElementById("instruction-text");
+const logo = document.getElementById("logo");
+const score = document.getElementById("score");
+const highScoreText = document.getElementById("high-score");
+
+const gridSize = 20;
+let snake = [{ x: 10, y: 10 }];
+let food = generateFood();
+let direction = generateFirstMove();
+let gameInterval;
+let gameSpeedDelay = 200;
+let gameStarted = false;
+let highScore = 0;
+
+function draw() {
+  board.innerHTML = "";
+  drawSnake();
+  drawFood();
+  updateScore();
+}
+
+function drawSnake() {
+  snake.forEach((segment) => {
+    const snakeElement = createGameElement("div", "snake");
+    setPosition(snakeElement, segment);
+    board.appendChild(snakeElement);
+  });
+}
+
+function createGameElement(tag, className) {
+  const element = document.createElement(tag);
+  element.className = className;
+  return element;
+}
+
+function setPosition(element, position) {
+  element.style.gridColumn = position.x;
+  element.style.gridRow = position.y;
+}
+
+function drawFood() {
+  if (gameStarted) {
+    const foodElement = createGameElement("div", "food");
+    setPosition(foodElement, food);
+    board.appendChild(foodElement);
+  }
+}
+
+function generateFood() {
+  const x = Math.floor(Math.random() * gridSize) + 1;
+  const y = Math.floor(Math.random() * gridSize) + 1;
+  return { x, y };
+}
+
+function move() {
+  const head = { ...snake[0] };
+  switch (direction) {
+    case "up":
+      head.y--;
+      break;
+    case "right":
+      head.x++;
+      break;
+    case "down":
+      head.y++;
+      break;
+    case "left":
+      head.x--;
+      break;
+  }
+  snake.unshift(head);
+
+  if (head.x === food.x && head.y === food.y) {
+    food = generateFood();
+    increaseSpeed();
+    clearInterval(gameInterval);
+    gameInterval = setInterval(() => {
+      move();
+      checkColission();
+      draw();
+    }, gameSpeedDelay);
+  } else {
+    snake.pop();
+  }
+}
+
+function generateFirstMove() {
+  let firstMove = Math.floor(Math.random() * 4) + 1;
+  switch (firstMove) {
+    case 1:
+      firstMove = "up";
+      break;
+    case 2:
+      firstMove = "right";
+      break;
+    case 3:
+      firstMove = "down";
+      break;
+    case 4:
+      firstMove = "left";
+      break;
+  }
+  return firstMove;
+}
+
+function startGame() {
+  gameStarted = true;
+  // instructionText.style.display = "none";
+  // logo.style.display = "none";
+  gameInterval = setInterval(() => {
+    move();
+    checkColission();
+    draw();
+  }, gameSpeedDelay);
+}
+
+function handleKeyPress(event) {
+  if (
+    (!gameStarted && event.code === "Space") ||
+    (!gameStarted && event.key === " ")
+  ) {
+    startGame();
+  } else {
+    switch (event.key) {
+      case "ArrowUp":
+        direction = "up";
+        break;
+      case "ArrowRight":
+        direction = "right";
+        break;
+      case "ArrowDown":
+        direction = "down";
+        break;
+      case "ArrowLeft":
+        direction = "left";
+        break;
+    }
+  }
+}
+
+document.addEventListener("keydown", handleKeyPress);
+
+function increaseSpeed() {
+  if (gameSpeedDelay > 150) {
+    gameSpeedDelay -= 5;
+  } else if (gameSpeedDelay > 100) {
+    gameSpeedDelay -= 3;
+  } else if (gameSpeedDelay > 50) {
+    gameSpeedDelay -= 2;
+  } else if (gameSpeedDelay > 25) {
+    gameSpeedDelay -= 1;
+  }
+}
+
+function checkColission() {
+  const head = snake[0];
+
+  if (head.x < 1 || head.x > gridSize || head.y < 1 || head.y > gridSize) {
+    resetGame();
+  }
+
+  for (i = 1; i < snake.length; i++) {
+    if (head.x === snake[i].x && head.y === snake[i].y) {
+      resetGame();
+    }
+  }
+}
+
+function resetGame() {
+  updateHighScore();
+  stopGame();
+  snake = [{ x: 10, y: 10 }];
+  food = generateFood();
+  direction = generateFirstMove();
+  gameSpeedDelay = 200;
+  updateScore();
+}
+
+function updateScore() {
+  const currentScore = snake.length - 1;
+  score.textContent = currentScore.toString().padStart(3, "0");
+}
+
+function stopGame() {
+  clearInterval(gameInterval);
+  gameStarted = false;
+  // instructionText.style.display = "block";
+  // logo.style.display = "block";
+}
+
+function updateHighScore() {
+  const currentScore = snake.length - 1;
+  if (currentScore > highScore) {
+    highScore = currentScore;
+    highScoreText.textContent = highScore.toString().padStart(3, "0");
+  }
+  highScoreText.style.display = "block";
+}
+
+// Info section
+
+let goToregister = document.querySelector("#go-to-register");
+let goToAuthDiv = document.querySelector("#go-to-auth");
+let infoSectionDiv = document.querySelector("#info-section");
+
+goToregister.addEventListener("click", () => {
+  infoSectionDiv.innerHTML = `
+  <h2>რეგისტრაცია</h2>
+   <form action="">
+      <label for="username">ჩაწერე იუზერი</label>
+      <input id="username" class="input-username" type="text" placeholder="username" required >
+      <label for="pass">ჩაწერე პაროლი</label>
+      <input id="pass" class="input-pass" type="password" placeholder="****" required >
+      <button class="submit-register">Submit</button>
+    </form>
+        `;
+});
+
+goToAuthDiv.addEventListener("click", () => {
+  infoSectionDiv.innerHTML = `
+    <h2>ავტორიზაცია</h2>
+     <form action="">
+      <label for="username">ჩაწერე იუზერი</label>
+      <input id="username" class="input-username" type="text" placeholder="username" required >
+      <label for="pass">ჩაწერე პაროლი</label>
+      <input id="pass" class="input-pass" type="password" placeholder="****" required >
+      <button class="submit-auth">Submit</button>
+    </form>
+  `;
+});
